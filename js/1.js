a49502f7d5c0bdb34cde403fddcbb93f6acc9bc1
AnhQuangCee <duonganhquang97@gmail.com>
2019-01-27 13:51:15 +0700

    Complete Coundown

--- js/1.js
+++ js/1.js
@@ -52,3 +52,24 @@ svgLogoAnimation.add({
 	duration: 2000,
 	easing: 'easeOutSine'
 });
+
+//DAQ Counntdown web 2019
+var countDownDate = new Date("Feb 5, 2019 00:00:00").getTime();
+var x = setInterval(function(){
+	var now = new Date().getTime();
+	var distance = countDownDate - now;
+
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	
+	document.getElementById("day").innerHTML = days;
+	document.getElementById("hours").innerHTML = hours;
+	document.getElementById("minutes").innerHTML = minutes;
+	document.getElementById("senconds").innerHTML = seconds;
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("demo").innerHTML = "HAPPY NEW YEAR 2019";
+	}
+})
